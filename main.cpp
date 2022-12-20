89129eea369b38834c323052308c9c91bf52d0d5
Samantha <samanthasofiabg@gmail.com>
2022-12-20 06:33:40 -0600

    Ligeros cambios

--- main.cpp
+++ main.cpp
@@ -1,18 +1,16 @@
 #include <iostream>
 #include <climits>
 #include <string>
+#include <map>
+#include <cmath>
 using namespace std;
 
-void resetBoard(char positions[])
-{
+void resetBoard(char positions[]) {
     for (int i = 0; i < 9; i++)
-    {
         positions[i] = 49 + i;
-    }
 }
 
-void showGrid(char positions[])
-{
+void showGrid(char positions[]) {
     cout << "       |       |       " << endl;
     cout << "   " << positions[0] << "   |   " << positions[1] << "   |   " << positions[2] << "   " << endl;
     cout << "_______|_______|_______" << endl;
@@ -24,41 +22,33 @@ void showGrid(char positions[])
     cout << "       |       |       " << endl;
 }
 
-int askPosition(char positions[], bool player)
-{
+int askPosition(char positions[], bool player) {
     showGrid(positions);
     string res;
 
     cout << "Player " << player + 1 << ": Position to play? (1-9)" << endl;
     cin >> res;
 
-    try
-    {
+    try {
         if (stoi(res) < 1 || stoi(res) > 9)
-        {
             throw 505;
-        }
 
-        if (positions[stoi(res) - 1] == 'O' || positions[stoi(res) - 1] == 'X')
-        {
+        if (positions[stoi(res) - 1] == 'O' || positions[stoi(res) - 1] == 'X') {
             cout << "Invalid response" << endl;
             return askPosition(positions, player);
         }
-        else
-        {
+        else {
             positions[stoi(res) - 1] = (player ? 'X' : 'O');
             return stoi(res) - 1;
         }
     }
-    catch (...)
-    {
+    catch (...) {
         cout << "Invalid response" << endl;
         return askPosition(positions, player);
     }
 }
 
-bool checkWin(char positions[], int lastPosition, bool player)
-{
+bool checkWin(char positions[], int lastPosition, bool player) {
     int combinations[9][4][2] = {
         {{1, 2}, {4, 8}, {3, 6}},
         {{0, 2}, {4, 7}},
@@ -68,171 +58,88 @@ bool checkWin(char positions[], int lastPosition, bool player)
         {{2, 8}, {3, 4}},
         {{0, 3}, {7, 8}, {2, 4}},
         {{1, 4}, {6, 8}},
-        {{2, 5}, {6, 7}, {0, 4}}};
+        {{2, 5}, {6, 7}, {0, 4}}
+    };
 
     int posibilities = lastPosition == 4 ? 4 : 3 - lastPosition % 2;
 
-    for (int i = 0; i < posibilities; i++)
-    {
+    for (int i = 0; i < posibilities; i++) {
         int total = 0;
-        for (int j = 0; j < 2; j++)
-        {
-            if (positions[combinations[lastPosition][i][j]] == (player ? 'X' : 'O'))
-            {
+        for (int j = 0; j < 2; j++) {
+            if (positions[combinations[lastPosition][i][j]] == (player ? 'X' : 'O')) 
                 total++;
-            }
             if (total == 2)
-            {
                 return true;
-            }
         }
     }
     return false;
 }
 
-// Regresa True si ya no hay posiciones disponibles para jugar, por ende si hay empate, false si no
-bool TerminaJuego(char positions[])
-{
-    for (int i = 0; i < 9; i++)
-    {
-        if (positions[i] == 'X' || positions[i] == 'O')
-        {
-            return false;
-        }
+int checkKey(char positions[]) {
+    int key = 0;
+    for (int i = 0; i < 9; i++) {
+        if (positions[i] == 'X')
+            key = key + ((int) pow(3.0, i) * 2);
+        else if (positions[i] == 'O')
+            key = key + ((int) pow(3.0, i));
     }
-    return true;
+    return key;
 }
 
-int evaluation(char positions[])
-{
-    // Revisamos las posibles convinaciones de victoria
-    //  -10 Si gana el jugador 1
-    //  +10 Si gana el jugador 2
-    //  +0 si no gana nadie, empate o el juego aun no termina
-    if (positions[0] == positions[1] && positions[1] == positions[2])
-    {
-        // cout << "Gano " << positions[0];
-        if (positions[1] == 'O')
-            return (-10);
-        else
-            return 10;
-    }
-    else if (positions[3] == positions[4] && positions[4] == positions[5])
-    {
-        // cout << "Gano " << positions[3];
-        if (positions[3] == 'O')
-            return -10;
-        else
-            return 10;
-    }
-    else if (positions[6] == positions[7] && positions[7] == positions[8])
-    {
-        // cout << "Gano " << positions[6];
-        if (positions[6] == 'O')
-            return -10;
-        else
-            return 10;
-    }
-    else if (positions[0] == positions[4] && positions[4] == positions[8])
-    {
-        // cout << "Gano " << positions[0];
-        if (positions[0] == 'O')
-            return -10;
-        else
-            return 10;
-    }
-    else if (positions[6] == positions[4] && positions[4] == positions[2])
-    {
-        // cout << "Gano " << positions[6];
-        if (positions[6] == 'O')
-            return -10;
-        else
-            return 10;
-    }
-    else if (positions[0] == positions[3] && positions[3] == positions[6])
-    {
-        // cout << "Gano " << positions[6];
-        if (positions[0] == 'O')
-            return -10;
-        else
-            return 10;
-    }
-    else if (positions[1] == positions[4] && positions[4] == positions[7])
-    {
-        // cout << "Gano " << positions[1];
-        if (positions[1] == 'O')
-            return -10;
-        else
-            return 10;
-    }
-    else if (positions[2] == positions[5] && positions[5] == positions[8])
-    {
-        // cout << "Gano " << positions[2];
-        if (positions[2] == 'O')
-            return -10;
-        else
-            return 10;
-    }
-    else
-    {
-        // cout << "No ha ganado Nadie" << endl;
-        return 0;
-    }
-}
+int minimax(char positions[], int depth, bool player, int lastPosition, map<int, int> &results, int plays) {
+    int key = checkKey(positions);
+    int score;
 
-int minimax(char positions[], int depth, bool player)
-{
-    int score = evaluation(positions);
+    if(results.find(key) == results.end()) {
+        score = checkWin(positions, lastPosition, !player) ? (!player ? 10 : -10) : 0;
+        if(score != 0)
+            results[key] = score;
+    } else 
+        score = results[key];
+
     // Si Gana el Bot/Humano
     if (score != 0)
         return score;
     // Empate
-    if (TerminaJuego(positions))
+    if (plays == 9)
         return 0;
     // si player es true, es el jugador 2 por ende el bot
-    if (player)
-    {
+    if (player) {
         int maximum = INT_MIN;
-        for (int i = 0; i < 9; i++)
-        {
-            if (positions[i] != 'X' && positions[i] != 'O')
-            {
+        for (int i = 0; i < 9; i++) {
+            if (positions[i] != 'X' && positions[i] != 'O') {
                 positions[i] = 'X';
-                int mx = minimax(positions, depth + 1, !player);
+                int mx = minimax(positions, depth + 1, !player, i, results, plays+1);
                 maximum = max(maximum, mx);
                 positions[i] = i + 49;
+                plays--;
             }
         }
         return maximum - depth;
     }
-    else
-    {
+    else {
         int minimum = INT_MAX;
-        for (int i = 0; i < 9; i++)
-        {
-            if (positions[i] != 'X' && positions[i] != 'O')
-            {
+        for (int i = 0; i < 9; i++) {
+            if (positions[i] != 'X' && positions[i] != 'O') {
                 positions[i] = 'O';
-                int mm = minimax(positions, depth + 1, !player);
+                int mm = minimax(positions, depth + 1, !player, i, results, plays+1);
                 minimum = min(minimum, mm);
                 positions[i] = i + 49;
+                plays--;
             }
         }
         return minimum - depth;
     }
 }
 
-int bestMove(char positions[])
-{
+int bestMove(char positions[], map<int, int> &results, int plays) {
     int bestVal = INT_MIN;
     int OptiMove;
-    for (int i = 0; i < 9; i++)
-    {
-        if (positions[i] != 'X' && positions[i] != 'O')
-        {
+    for (int i = 0; i < 9; i++) {
+        if (positions[i] != 'X' && positions[i] != 'O') {
             positions[i] = 'X';
 
-            int ValMove = minimax(positions, 0, false);
+            int ValMove = minimax(positions, 0, false, i, results, plays);
             positions[i] = i + 49;
 
             if (ValMove > bestVal)
@@ -246,138 +153,70 @@ int bestMove(char positions[])
     return OptiMove;
 }
 
-// Falta funcionar para cuando el humano empiece
-
-void onePlayer(char positions[])
-{
+void game (int numberPlayer) {
     bool win = false, player = false;
     int plays = 0, lastPosition;
-    string again;
+    string again = "1";
 
-    play:
-
-    while (!win && plays < 9)
-    {
-
-
-        if (player)
-            lastPosition = bestMove(positions);
-        else
-            lastPosition = askPosition(positions, player);
-
-        win = checkWin(positions, lastPosition, player);
-        player = !player;
-        plays++;
-    }
-
-    if (win)
-    {
-        showGrid(positions);
-        player = !player;
-
-        cout << endl
-             << "Player " << player + 1 << " won!" << endl;
-    }
-    else
-    {
-        cout << endl
-             << "It was a tie!" << endl;
-    }
-
-    cout<<"Do you want to play again? (1/2)"<<endl;
-    cin>>again;
-    if(again=="1")
-    {
-        resetBoard(positions);
-        player = !player;
-        plays = 0; win = false;
-        goto play;
-    }
-
-
-    cout << "Thanks for playing!" << endl;
-}
-
-void twoPlayers(char positions[])
-{
-    bool win = false, player = false;
-    int plays = 0;
-    string again;
-
-    play:
-
-
-    while (!win && plays < 9)
-    {
-        int lastPosition = askPosition(positions, player);
-        win = checkWin(positions, lastPosition, player);
-        player = !player;
-        plays++;
-    }
-
-    if (win)
-    {
-        showGrid(positions);
-        player = !player;
-
-        cout << endl
-             << "Player " << player + 1 << " won!" << endl;
-    }
-    else
-    {
-        cout << endl
-             << "It was a tie!" << endl;
-    }
-    
-    
-    cout<<"Do you want to play again? (1/2)"<<endl;
-    cin>>again;
-    if(again=="1")
-    {
-        resetBoard(positions);
-        player = !player;
-        plays = 0; win = false;
-        goto play;
-    }
-    cout << "Thanks for playing!" << endl;
-}
-
-void playerNumbers()
-{
-    string res;
+    map<int, int> results; 
 
     char positions[9];
 
     for (int i = 0; i < 9; i++)
-    {
         positions[i] = i + 49;
+
+    while (again == "1") {
+        while (!win && plays < 9) {
+            if (player)
+                if (!numberPlayer)
+                    lastPosition = bestMove(positions, results, plays);
+                else 
+                    lastPosition = askPosition(positions, player);
+            else 
+                lastPosition = askPosition(positions, player);
+
+            win = checkWin(positions, lastPosition, player);
+            player = !player;
+            plays++;
+        }
+
+        if (win) {
+            showGrid(positions);
+            player = !player;
+
+            cout << endl
+                << "Player " << player + 1 << " won!" << endl;
+        }
+        else {
+            cout << endl
+                << "It was a tie!" << endl;
+        }
+
+        cout<<"Do you want to play again? (1/2)"<<endl;
+        cin>>again;
+
+        resetBoard(positions);
+        player = !player;
+        plays = 0; win = false;
     }
 
-    cout << "How many players? (1/2)" << endl;
-    cin >> res;
-
-    if (res == "1")
-        onePlayer(positions);
-    else if (res == "2")
-        twoPlayers(positions);
-    else if (res == "exit")
-        cout << "Thanks for playing!" << endl;
-    else
-    {
-        cout << "Invalid response" << endl
-             << endl;
-        playerNumbers();
-    }
+    cout << "Thanks for playing!" << endl;
 }
 
-int main()
-{
+int main() {
     cout << "---------------------------------------------" << endl;
     cout << "TIC TAC TOE" << endl;
     cout << "----------------------------------------------" << endl;
     cout << endl;
 
-    playerNumbers();
+    string res;
+    cout << "How many players? (1/2)" << endl;
+    cin >> res;
+
+    if (res == "1" || res == "2")
+        game(stoi(res) - 1);
+    else 
+        cout << "Invalid response" << endl;
 
     return 0;
 }
